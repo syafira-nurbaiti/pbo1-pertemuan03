7ff0118eaceb0c0e901cd70d6edf944b2edf1a72
syafira-nurbaiti <cosmic947@gmail.com>
2020-05-01 16:27:07 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -7,4 +7,13 @@ public class OperatorBinary {
     String nama = "Syafira Nurbaiti";
     int jumlahSks = 24;
     double indeksPresentasiKumulatif = 3.89;
+
+    public static void main(String[] args ){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
