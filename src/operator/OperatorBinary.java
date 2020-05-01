4c2fafe6fa29fd6fdb3f0e253dd3a9f25d9c6629
syafira-nurbaiti <cosmic947@gmail.com>
2020-05-01 16:25:49 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+
+    String nama = "Syafira Nurbaiti";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.89;
+}
